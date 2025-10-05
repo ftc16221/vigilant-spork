e674d11366f44bee3f1f5fe5b6101555ad539155
milesavernon <milesavernon@gmail.com>
2025-10-04 21:02:54 -0400

    added semiAuto program to test out

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/TestAuto.java
@@ -43,7 +43,7 @@ public class TestAuto extends LinearOpMode {
                         PATH_1.reinitialize();
                     }
                 }
-
+                poseTracker.update();
             }
         }
     }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/SemiAutoTeleOp.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subassemblies.MecDriveBase;
+import org.firstinspires.ftc.teamcode.subassemblies.Underglow;
+import org.firstinspires.ftc.teamcode.subassemblies.autonomous.PoseTracker;
+import org.firstinspires.ftc.teamcode.util.Global;
+import org.firstinspires.ftc.teamcode.util.Pose;
+
+@TeleOp
+@Config
+public class SemiAutoTeleOp extends LinearOpMode {
+
+    public static Pose TARGET_POSE = new Pose(0, 0, 0);
+
+    public void runOpMode() {
+        MecDriveBase driveBase = new MecDriveBase(this);
+        PoseTracker poseTracker = new PoseTracker(this, Global.lastPose);
+        Underglow underglow = new Underglow(this);
+
+        poseTracker.setTargetPose(TARGET_POSE);
+
+        if (opModeIsActive()) {
+            while(opModeIsActive()) {
+                driveBase.moveRobot(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x);
+                if (gamepad1.right_bumper) {
+                    poseTracker.enableMovement();
+                    underglow.setColor(Underglow.Color.WHITE);
+                } else if (gamepad1.right_trigger > 0.1) {
+                    poseTracker.disableMovement();
+                    underglow.setColor(Underglow.Color.ALLIANCE);
+                }
+                poseTracker.update();
+            }
+        }
+    }
+}

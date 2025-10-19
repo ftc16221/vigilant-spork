c40e43624362b689d94c0ba8955b09e615881906
Miles Vernon <149839790+MilesVernon@users.noreply.github.com>
2025-10-19 12:11:29 -0400

    fix some copilot criticisms

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/BasicTeleOp.java
@@ -1,24 +1,14 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.canvas.Canvas;
-import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.subassemblies.Intake;
 import org.firstinspires.ftc.teamcode.subassemblies.MecDriveBase;
 import org.firstinspires.ftc.teamcode.subassemblies.Underglow;
-import org.firstinspires.ftc.teamcode.subassemblies.autonomous.PoseTracker;
-import org.firstinspires.ftc.teamcode.util.DashOpMode;
-import org.firstinspires.ftc.teamcode.util.Drawing;
-import org.firstinspires.ftc.teamcode.util.Global;
-import org.firstinspires.ftc.teamcode.util.Pose;
 
 @TeleOp
-@Config
-public class BasicTeleOp extends LinearOpMode implements DashOpMode {
+public class BasicTeleOp extends LinearOpMode {
 
     public void runOpMode() {
         MecDriveBase driveBase = new MecDriveBase(this);
@@ -38,7 +28,7 @@ public class BasicTeleOp extends LinearOpMode implements DashOpMode {
                 } else if (gamepad1.dpad_down || gamepad2.y) {
                     intake.run(Intake.Direction.OUT);
                 } else if (gamepad1.dpad_left || gamepad1.dpad_right || gamepad2.b) {
-                    intake.stop();
+                    intake.stop(); 
                 }
                 // UPDATES
                 telemetry.update();

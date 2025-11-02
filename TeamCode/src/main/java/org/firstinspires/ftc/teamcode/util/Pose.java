823124098b6b0c8af0b9e2d2b5baffceaaebe9f4
Miles Vernon <149839790+MilesVernon@users.noreply.github.com>
2025-11-02 08:49:13 -0500

    added a load of useful comparison and conversion methods to Pose.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Pose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Pose.java
@@ -1,10 +1,14 @@
 package org.firstinspires.ftc.teamcode.util;
 
+import androidx.annotation.NonNull;
+
 import com.acmerobotics.dashboard.canvas.Canvas;
 import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
+
+import java.util.Objects;
 
 public class Pose {
     public double x, y , h;
@@ -31,4 +35,31 @@ public class Pose {
     public SparkFunOTOS.Pose2D toSparkFunPose() {
         return new SparkFunOTOS.Pose2D(x, y, h);
     }
+
+    @NonNull
+    public String toString() { return String.format("Pose(x=%.2f, y=%.2f, h=%.2f", x, y, h); }
+
+    public int hashCode() { return Objects.hash(x, y, h); }
+
+    public boolean equals(Pose pose) { return this.hashCode() == pose.hashCode(); }
+
+    public Pose subtract(Pose subtrahend) {
+        return new Pose(
+                this.x - subtrahend.x,
+                this.y - subtrahend.y,
+                this.h - subtrahend.h
+        );
+    }
+
+    public Pose add(Pose addend) {
+        return new Pose(
+                this.x + addend.x,
+                this.y + addend.y,
+                this.h + addend.h
+        );
+    }
+
+    public double distanceFromOrigin() { return Math.hypot(x, y); }
+
+    public double distanceFrom(Pose pose) { return subtract(pose).distanceFromOrigin(); }
 }

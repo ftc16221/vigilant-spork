853b5b6bf191d1390744d000efc74dd31529d23a
Miles Vernon <149839790+MilesVernon@users.noreply.github.com>
2025-11-11 19:00:40 -0500

    changed superclass from LinearOpMode to OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/SemiAutoTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/SemiAutoTeleOp.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
 import com.acmerobotics.dashboard.config.Config;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
@@ -9,6 +9,8 @@ import org.firstinspires.ftc.teamcode.subassemblies.Intake;
 import org.firstinspires.ftc.teamcode.subassemblies.MecDriveBase;
 import org.firstinspires.ftc.teamcode.subassemblies.Underglow;
 import org.firstinspires.ftc.teamcode.subassemblies.autonomous.PoseTracker;
+import org.firstinspires.ftc.teamcode.subassemblies.autonomous.localizers.LimelightCam;
+import org.firstinspires.ftc.teamcode.subassemblies.autonomous.localizers.PinpointOdo;
 import org.firstinspires.ftc.teamcode.util.DashOpMode;
 import org.firstinspires.ftc.teamcode.util.Drawing;
 import org.firstinspires.ftc.teamcode.util.Global;
@@ -16,7 +18,7 @@ import org.firstinspires.ftc.teamcode.util.Pose;
 
 @TeleOp(group = Global.OpModeGroup.MAIN)
 @Config
-public class SemiAutoTeleOp extends LinearOpMode implements DashOpMode {
+public class SemiAutoTeleOp extends OpMode implements DashOpMode {
 
     public static Pose TARGET_POSE = new Pose(0, 0, 0);
     public static Pose TRACKING_POINT = new Pose(0, 0, 0);
@@ -25,69 +27,76 @@ public class SemiAutoTeleOp extends LinearOpMode implements DashOpMode {
     public static Underglow.Color GOAL_TRACKING_COLOR = Underglow.Color.GREEN;
     public static Underglow.Color AUTO_MOVEMENT_COLOR = Underglow.Color.WHITE;
 
+    MecDriveBase driveBase;
+    Intake intake;
+    Underglow underglow;
+    PoseTracker poseTracker;
+    LimelightCam limelightCam;
+    PinpointOdo pinpointOdo;
+    Drawing drawing;
+
     private boolean autoMovementEnabled = false;
     private boolean goalTrackingEnabled = false;
 
-    public void runOpMode() {
-        MecDriveBase driveBase = new MecDriveBase(this);
-        Intake intake = new Intake(this);
-        PoseTracker poseTracker = new PoseTracker(this, Global.lastPose);
-        Underglow underglow = new Underglow(this);
-        Drawing drawing = new Drawing(poseTracker);
+    @Override
+    public void init() {
+        driveBase = new MecDriveBase(this);
+        intake = new Intake(this);
+        limelightCam = new LimelightCam(this);
+        pinpointOdo = new PinpointOdo(this, Global.lastPose);
+        poseTracker = new PoseTracker(this, Global.lastPose);
+        underglow = new Underglow(this);
+        drawing = new Drawing(poseTracker);
 
         driveBase.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         poseTracker.setTargetPose(TARGET_POSE);
         poseTracker.setTrackingPoint(TRACKING_POINT);
         poseTracker.setControllerType(PoseTracker.ControllerType.APPROACH);
+    }
 
-        telemetry.update();
-        waitForStart();
+    @Override
+    public void loop() {
 
-        if (opModeIsActive()) {
-            while(opModeIsActive()) {
-                // INTAKE
-                if (gamepad1.dpad_up || gamepad2.a) {
-                    intake.run(Intake.Direction.IN);
-                } else if (gamepad1.dpad_down || gamepad2.y) {
-                    intake.run(Intake.Direction.OUT);
-                } else if (gamepad1.dpad_left || gamepad1.dpad_right || gamepad2.b) {
-                    intake.stop();
-                }
-
-                // ALL DRIVEBASE MOVEMENT (SEMI-AUTO OR TELEOP)
-                if (gamepad1.right_bumper) { // start auto movement
-                    autoMovementEnabled = true;
-                    poseTracker.enableMovement();
-                    underglow.setColor(AUTO_MOVEMENT_COLOR);
-                } else if (!gamepad1.atRest() && autoMovementEnabled) { // cancel auto movement
-                    autoMovementEnabled = false;
-                    poseTracker.disableMovement();
-                    underglow.setColor(IDLE_COLOR);
-                } else if (gamepad1.left_bumper || gamepad1.right_stick_button) { // start goal tracking
-                    goalTrackingEnabled = true;
-                    poseTracker.enablePointTracking();
-                    underglow.setColor(GOAL_TRACKING_COLOR);
-                } else if (gamepad1.right_stick_x != 0 && goalTrackingEnabled) { // cancel goal tracking
-                    goalTrackingEnabled = false;
-                    poseTracker.disablePointTracking();
-                    underglow.setColor(IDLE_COLOR);
-                }
-
-                if (goalTrackingEnabled) {
-                    driveBase.moveRobot(gamepad1.left_stick_x, -gamepad1.left_stick_y, poseTracker.getTrackingPower());
-                }
-                if (!autoMovementEnabled && !goalTrackingEnabled) {
-                    driveBase.control(gamepad1);
-                }
-
-                // UPDATES
-                poseTracker.update();
-                poseTracker.runTelemetry();
-                telemetry.update();
-                drawing.update();
-            }
+        // INTAKE
+        if (gamepad1.dpad_up || gamepad2.a) {
+            intake.run(Intake.Direction.IN);
+        } else if (gamepad1.dpad_down || gamepad2.y) {
+            intake.run(Intake.Direction.OUT);
+        } else if (gamepad1.dpad_left || gamepad1.dpad_right || gamepad2.b) {
+            intake.stop();
         }
 
+        // ALL DRIVEBASE MOVEMENT (SEMI-AUTO OR TELEOP)
+        if (gamepad1.right_bumper) { // start auto movement
+            autoMovementEnabled = true;
+            poseTracker.enableMovement();
+            underglow.setColor(AUTO_MOVEMENT_COLOR);
+        } else if (!gamepad1.atRest() && autoMovementEnabled) { // cancel auto movement
+            autoMovementEnabled = false;
+            poseTracker.disableMovement();
+            underglow.setColor(IDLE_COLOR);
+        } else if (gamepad1.left_bumper || gamepad1.right_stick_button) { // start goal tracking
+            goalTrackingEnabled = true;
+            poseTracker.enablePointTracking();
+            underglow.setColor(GOAL_TRACKING_COLOR);
+        } else if (gamepad1.right_stick_x != 0 && goalTrackingEnabled) { // cancel goal tracking
+            goalTrackingEnabled = false;
+            poseTracker.disablePointTracking();
+            underglow.setColor(IDLE_COLOR);
+        }
+
+        if (goalTrackingEnabled) {
+            driveBase.moveRobot(gamepad1.left_stick_x, -gamepad1.left_stick_y, poseTracker.getTrackingPower());
+        }
+        if (!autoMovementEnabled && !goalTrackingEnabled) {
+            driveBase.control(gamepad1);
+        }
+
+        // UPDATES
+        poseTracker.update();
+        poseTracker.runTelemetry();
+        telemetry.update();
+        drawing.update();
     }
 }

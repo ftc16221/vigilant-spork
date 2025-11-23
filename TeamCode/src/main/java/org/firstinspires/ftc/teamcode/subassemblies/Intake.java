d8e61a5e2d55c9519f6cb1af7c0e4ca62b355fa7
milesavernon <milesavernon@gmail.com>
2025-11-22 20:53:07 -0500

    updated ftc dashboard to [v0.5.1](https://github.com/acmerobotics/ftc-dashboard/releases/tag/v0.5.1), which also lets us remove our workaround for the CRServo crash that would happen in ftc dash.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subassemblies/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subassemblies/Intake.java
@@ -3,30 +3,30 @@ package org.firstinspires.ftc.teamcode.subassemblies;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.util.Subassembly;
 
 @Config
 public class Intake extends Subassembly {
 
-    private final Servo intakeServo;
+    public static double SERVO_POWER = 0.8;
+
+    private final CRServo intakeServo;
 
     public Intake(OpMode opMode) {
         super(opMode, "Intake");
         HardwareMap hardwareMap = opMode.hardwareMap;
-        intakeServo = hardwareMap.servo.get("intake");
+        intakeServo = hardwareMap.crservo.get("intake");
     }
 
     public void run(Direction direction) {
-        if (direction == Direction.IN) intakeServo.setPosition(1);
-        else if (direction == Direction.OUT) intakeServo.setPosition(0);
+        if (direction == Direction.IN) intakeServo.setPower(SERVO_POWER);
+        else if (direction == Direction.OUT) intakeServo.setPower(-SERVO_POWER);
     }
 
     public void stop() {
-        intakeServo.setPosition(0.5);
+        intakeServo.setPower(0);
     }
 
     public enum Direction {
